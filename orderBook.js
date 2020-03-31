c4b3d7ad023f4e333de51728684cea8e8dc978a1
Wawesh2020 <ghenry429@gmail.com>
2020-03-31 12:34:58 -0400

    Still working on orderBook

---
+++ orderBook.js
@@ -0,0 +1,24 @@
+function reconcileOrder(book){
+  let order = 0
+  let existing = []
+
+  for(let i = 0; i < book.length;i++){
+    if(book[i] < 0){
+      order = book[i].push()
+    }
+    return existing
+  }
+  if(book[i] === existing){
+    order = book[i].push()
+  }
+
+  if(book[i] != existing){
+    order = book[i].push()
+  }
+
+    
+}
+
+
+
+module.exports = reconcileOrder
