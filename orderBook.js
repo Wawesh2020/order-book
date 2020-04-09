b46e290ec07aabaf2df4c4409e09aa8c3f5ce5a3
Wawesh2020 <ghenry429@gmail.com>
2020-04-09 17:31:23 -0400

    Redid reconcileOrder function  for all tests to run effectively

--- orderBook.js
+++ orderBook.js
@@ -1,24 +1,41 @@
-function reconcileOrder(book){
-  let order = 0
-  let existing = []
+const isMatching = (existingOrder, incomingOrder) => incomingOrder.type === 'buy'
+  ? incomingOrder.price >= existingOrder.price
+  : incomingOrder.price <= existingOrder.price
 
-  for(let i = 0; i < book.length;i++){
-    if(book[i] < 0){
-      order = book[i].push()
+const isMatchingType = (existingOrder, incomingOrder) =>
+  existingOrder.type !== incomingOrder.type && isMatching(existingOrder, incomingOrder)
+
+
+const isFulfill = (matchingOrder, incomingOrder) => matchingOrder.quantity <= incomingOrder.quantity
+  ? { ...incomingOrder, quantity: incomingOrder.quantity - matchingOrder.quantity }
+  : { ...matchingOrder, quantity: matchingOrder.quantity - incomingOrder.quantity }
+
+const reconcileOrder = (existingOrder, incomingOrder) =>
+{
+  let updateBook = []
+
+  while (existingOrder.length && incomingOrder.quantity > 0)
+  {
+    const bookOrder = existingOrder.shift() 
+
+    if (isMatchingType(bookOrder, incomingOrder) && incomingOrder.quantity > 0)
+    {
+      incomingOrder = isFulfill(bookOrder, incomingOrder)
+      console.log(incomingOrder)
+    } else
+    {
+
+      updateBook.push(bookOrder)
     }
-    return existing
-  }
-  if(book[i] === existing){
-    order = book[i].push()
   }
 
-  if(book[i] != existing){
-    order = book[i].push()
+  updateBook = updateBook.concat(existingOrder)
+
+  if (incomingOrder.quantity > 0)
+  {
+    updateBook.push(incomingOrder)
   }
 
-    
+  return updateBook
 }
-
-
-
 module.exports = reconcileOrder
